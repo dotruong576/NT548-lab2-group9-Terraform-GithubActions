18dec13c724ab4718c53eaf126fb68950da5dec9
tNE1K <trungkiennguyen2003@gmail.com>
2024-11-14 00:34:14 +0700

    test12

--- backend.tf
+++ backend.tf
@@ -1,12 +1,12 @@
-terraform {
-    backend "s3" {
-    bucket = "s3statebackend-group9"
-    dynamodb_table = "state-lock"
-    key = "global/mystatefile/terraform.tfstate"
-    region = "us-east-1"
-    encrypt = true
-  }
-}
+# terraform {
+#     backend "s3" {
+#     bucket = "s3statebackend-group9"
+#     dynamodb_table = "state-lock"
+#     key = "global/mystatefile/terraform.tfstate"
+#     region = "us-east-1"
+#     encrypt = true
+#   }
+# }
 
 
 
