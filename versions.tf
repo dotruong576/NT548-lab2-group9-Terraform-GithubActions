45cfde63ea6dc0afd6e660d85e77f9fc67cf7fe6
tNE1K <trungkiennguyen2003@gmail.com>
2024-11-14 00:06:30 +0700

    test6

--- versions.tf
+++ versions.tf
@@ -1,6 +1,14 @@
 terraform {
   required_version = ">= 1.0"
 
+  backend "s3" {
+    bucket = "s3statebackend-group9"
+    dynamodb_table = "state-lock"
+    key = "global/mystatefile/terraform.tfstate"
+    region = "us-east-1"
+    encrypt = true
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
